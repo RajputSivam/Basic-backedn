05a8c37d59cca8d195d5db909c4e5de2bb43d1b5
Shivam Rajput <srlrajput9389@gmail.com>
2026-05-30 23:38:36 +0530

    first commit

--- app.js
+++ app.js
@@ -1,57 +1,29 @@
-//ORM-Object Relational Mapping
-//ODM-Object Document Mapping
-
 const express = require('express');
 const app = express();
-
-const userMode = require('./usermodel');
+const path = require('path');
+const userModel = require('./models/user');
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(require('path').join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send("welcome");
+    res.render("index");
 })
-app.get('/create', async (req, res) => {
-    let user = await userMode.create({
-        name: "Harsh",
-        email: "harash@gami .com",
-        username: "harsh"
+
+app.get('/read', async (req, res) => {
+    let users = await userModel.find()
+    res.render("read", { users });
+})
+
+app.post('/create', async (req, res) => {
+    let { name, email, image } = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
     });
-    res.send(user);
+    res.redirect('/read');
 });
-app.get('/create2', async (req, res) => {
-    let user = await userMode.create({
-        name: "Harshita",
-        email: "harashia@gami.com",
-        username: "harshita"
-    });
-    res.send(user);
-});
-
-app.get('/update', async (req, res) => {
-    let updateduser = await userMode.findOneAndUpdate({ username: 'harsh' }, { name: "harsh vandana sharma" }, { returnDocument: 'after' })
-    res.send(updateduser);
-})
-
-app.get("/read", async (req, res) => {
-    let users = await userMode.find();
-    res.send(users);
-})
-app.get("/read2", async (req, res) => {
-    let users = await userMode.findOne({ username: "harsh" });
-    res.send(users);
-})
-
-app.get("/delete", async (req, res) => {
-    let users = await userMode.findOneAndDelete({ username: "harsh" });
-    res.send(users);
-})
 app.listen(3000);
-
-
-
-// node -e "const mongoose = require('mongoose'); mongoose.connect('mongodb://localhost:27017/users').then(() => console.log('Connected')).catch(() => console.log('Not Connected'))"
-
